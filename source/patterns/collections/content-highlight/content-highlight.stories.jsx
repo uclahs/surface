1afc1a344aa25eddf12159d9d583a840e4943708
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-03-12 14:18:47 -0700

    Updates to card and highlight

--- source/patterns/collections/content-highlight/content-highlight.stories.jsx
+++ source/patterns/collections/content-highlight/content-highlight.stories.jsx
@@ -4,9 +4,9 @@ import highlight from './content-highlight.twig';
 import data from './content-highlight.yml';
 
 // Data fields between events and articles vary a little and for this reason
-// I am creating a second yml file for the article card specifically while
-// still rendering the component with the same markup from twig.
-// import dataArticle from './featured-card-article.yml';
+// I am creating a second yml file for the Article Highlight specifically
+// while still rendering the component with the same markup from twig.
+import dataArticle from './content-highlight-article.yml';
 
 const settings = {
   title: 'Collections/Content Highlight',
@@ -15,10 +15,16 @@ const settings = {
   render: args => parse(highlight(args)),
 };
 
-const Default = {
+const EventHighlight = {
   name: 'Event Highlight',
   args: { ...data },
 };
 
+const ArticleHighlight = {
+  name: 'Article Highlight',
+  args: { ...dataArticle },
+  render: args => parse(highlight(args)),
+};
+
 export default settings;
-export { Default };
+export { EventHighlight, ArticleHighlight };
