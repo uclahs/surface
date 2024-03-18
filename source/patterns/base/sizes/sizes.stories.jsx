9a74b37636cab651e8c92d019dccd00ecf26c108
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-03-18 13:44:47 -0700

    Updates class names for scafolding purposes

--- source/patterns/base/sizes/sizes.stories.jsx
+++ source/patterns/base/sizes/sizes.stories.jsx
@@ -1,14 +1,17 @@
 import parse from 'html-react-parser';
 
 import sizes from './sizes.twig';
+import data from './sizes.yml';
 
 const settings = {
   title: 'Base/Sizes',
+  args: { ...data },
 };
 
 const Sizes = {
   name: 'Sizes',
   render: (args) => parse(sizes(args)),
+  args: { ...data },
 };
 
 export default settings;
--- source/patterns/base/tables/tables.stories.jsx
+++ source/patterns/base/tables/tables.stories.jsx
@@ -7,11 +7,29 @@ const settings = {
   title: 'Base/Tables',
 };
 
-const Tables = {
-  name: 'Tables',
+const Base = {
+  name: 'Base',
   render: (args) => parse(tables(args)),
   args: { ...data },
 };
 
+const Primary = {
+  name: 'Table Primary',
+  render: (args) => parse(tables(args)),
+  args: {
+    ...data,
+    modifier: 'table--primary',
+  },
+};
+
+const Borderless = {
+  name: 'Table without border',
+  render: (args) => parse(tables(args)),
+  args: {
+    ...data,
+    modifier: 'table--hover',
+  },
+};
+
 export default settings;
-export { Tables };
+export { Base, Primary, Borderless };
