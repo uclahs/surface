a2e19b136ab7a1a5c627a30a7d8203037b58f030
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-03-19 09:29:33 -0700

    Adds background color to media quote

--- source/patterns/components/media-quote/media-quote.stories.jsx
+++ source/patterns/components/media-quote/media-quote.stories.jsx
@@ -1,5 +1,8 @@
 import parse from 'html-react-parser';
 
+// Imports decorators for background colors.
+import { SantaBarbaraSandBg, VeniceCanalBg } from '../../../../.storybook/decorators';
+
 import quote from './media-quote.twig';
 import data from './media-quote.yml';
 
@@ -14,6 +17,17 @@ const Quote = {
   args: { ...data },
 };
 
+const SantaBarbaraSand = {
+  name: 'With Santa Barbara Sand Background',
+  args: { ...data },
+  decorators: [SantaBarbaraSandBg],
+};
+
+const VeniceCanal = {
+  name: 'With Venice Canal Background',
+  args: { ...data },
+  decorators: [VeniceCanalBg],
+};
 
 export default settings;
-export { Quote };
+export { Quote, SantaBarbaraSand, VeniceCanal };
