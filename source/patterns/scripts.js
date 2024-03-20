a165b249aa3ae8d11b91a0689e16561f4c41001a
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-03-20 11:35:15 -0700

    Imports all JS into scripts.js

--- source/patterns/scripts.js
+++ source/patterns/scripts.js
@@ -1,1 +1,4 @@
+import './elements/button-search/button-search';
+import './elements/messages/messages';
 import './components/accordion/accordion';
+import './components/accordion-tabs/accordion-tabs.min';
