8fbd87fdcfd67fe523e9dc2be65318d852e507ff
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-03-25 18:40:20 -0700

    Adds menu-sidebar menu and story

--- source/patterns/scripts.js
+++ source/patterns/scripts.js
@@ -6,3 +6,4 @@ import './components/accordion-tabs/accordion-tabs.min';
 import './components/menu-main/menu-main';
 import './components/menu-main/menu-utils';
 import './components/menu-main/menu-main-second-level';
+import './components/menu-sidebar/menu-sidebar';
