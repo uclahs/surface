35e0689061ea187506350f2cf31be48e546a69d4
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-03-26 14:05:36 -0700

    Imports tabs js

--- source/patterns/scripts.js
+++ source/patterns/scripts.js
@@ -7,4 +7,5 @@ import './components/menu-main/menu-main';
 import './components/menu-main/menu-utils';
 import './components/menu-main/menu-main-second-level';
 import './components/menu-sidebar/menu-sidebar';
+import './components/tabs/tabs';
 import './components/typewriter/typewriter';
