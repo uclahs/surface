3299bdf11a76f8e099d733ace6426cc10517bb20
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-04-01 14:41:09 -0700

    Adds slider and slider-item components

---
+++ source/patterns/collections/slider/slider.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+'use strict';
+
+((Drupal) => {
+  Drupal.behaviors.surfaceSlider = {
+    attach: function attach() {
+      Drupal.surfaceSlider.init();
+    },
+  };
+
+  Drupal.surfaceSlider = {
+    init: function () {
+      const surfaceSlider = document.querySelectorAll('.glide');
+      surfaceSlider.forEach((slider) => {
+        const glide = new Glide(slider, {
+          type: 'slide'
+        });
+        glide.mount();
+      });
+    },
+  };
+})(Drupal);
