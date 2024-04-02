2432667700bab2ac851981c37a74b6ba64cfce58
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-04-02 15:13:06 -0700

    Adds many forms related components and renames an existing component

---
+++ source/patterns/base/forms/form-boolean/form-boolean.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* eslint-disable */
+
+((Drupal) => {
+  Drupal.theme.checkbox = function () {
+    return '<input type="checkbox" class="form-checkbox form-boolean form-boolean--type-checkbox"/>';
+  };
+})(Drupal);
