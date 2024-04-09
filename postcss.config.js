f0e78a6ef54b659c258192b60305ef5867813ab2
Chaz Chumley <chaz.chumley@gmail.com>
2024-04-08 22:24:45 -0400

    fix nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 import postcssImport from 'postcss-import';
 import postcssImportExtGlob from 'postcss-import-ext-glob';
 import postcssGlobalData from '@csstools/postcss-global-data';
+import postcssNested from 'postcss-nested';
 import postcssPresetEnv from 'postcss-preset-env';
 
 export default {
@@ -10,6 +11,7 @@ export default {
     postcssGlobalData({
       files: ['./source/props/media.css'],
     }),
+    postcssNested(),
     postcssPresetEnv({
       stage: 4,
       features: {
