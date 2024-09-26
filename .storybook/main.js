1cba0e9ee44120aba599481eb494ba8ff3134021
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-09-26 08:45:12 -0700

    Removes Axe and A11y packages as they were causing large chunks in bundles

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
     '@storybook/addon-themes'
   ],
   framework: {
